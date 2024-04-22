6dca1ecd5523499b0f9688ea8dc0590d2cf5b195
Your name <Your email>
2024-04-22 10:47:15 +0200

    more

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 const OWM_API_KEY = process.env.OWM_API_KEY;
 
-
+// get movies
 router.get('/movies', (req, res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${OWM_API_KEY}`)
         .then(response => response.json())
@@ -15,6 +15,7 @@ router.get('/movies', (req, res) => {
 
 });
 
+// get tv shows
 router.get('/tv', (req, res) => {
     fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${OWM_API_KEY}`)
         .then(response => response.json())
@@ -24,12 +25,23 @@ router.get('/tv', (req, res) => {
         })
 });
 
+// get genres
 router.get('/genres', (req, res) => {
     fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${OWM_API_KEY}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
             res.json({ genres: data.genres });
+        })
+});
+
+// get top rated movies
+router.get('/topratedmovies', (req, res) => {
+    fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${OWM_API_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            res.json({ topratedmovies: data.results });
         })
 });
 

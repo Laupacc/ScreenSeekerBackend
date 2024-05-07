a101a76953cbd923aea41aeffdac5480384538fa
Your name <Your email>
2024-05-07 16:23:22 +0200

    c

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+    origin: 'https://my-movies-frontend-six.vercel.app'
+}));
 
 app.use(logger('dev'));
 app.use(express.json());

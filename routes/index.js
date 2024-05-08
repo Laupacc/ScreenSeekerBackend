15149120c0391cff9b824b5271922aee19dffef0
Your name <Your email>
2024-05-08 10:56:13 +0200

    api

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 const fetch = require('node-fetch');
 
-const OWM_API_KEY = process.env.OWM_API_KEY;
+const MOVIES_API_KEY = process.env.MOVIES_API_KEY;
 
 // get movies
 router.get('/movies', async (req, res) => {
     let movies = [];
     for (let i = 1; i <= 5; i++) {
-        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?language=en-US?api_key=${OWM_API_KEY}&page=${i}`);
+        const response = await fetch(`https://api.themoviedb.org/3/discover/movie?language=en-US?api_key=${MOVIES_API_KEY}&page=${i}`);
         const data = await response.json();
         movies = movies.concat(data.results);
     }
@@ -19,7 +19,7 @@ router.get('/movies', async (req, res) => {
 router.get('/tv', async (req, res) => {
     let tv = [];
     for (let i = 1; i <= 5; i++) {
-        const response = await fetch(`https://api.themoviedb.org/3/discover/tv?language=en-US?api_key=${OWM_API_KEY}&page=${i}`)
+        const response = await fetch(`https://api.themoviedb.org/3/discover/tv?language=en-US?api_key=${MOVIES_API_KEY}&page=${i}`)
         const data = await response.json();
         tv = tv.concat(data.results);
     }
@@ -28,7 +28,7 @@ router.get('/tv', async (req, res) => {
 
 // get genres
 router.get('/genres', async (req, res) => {
-    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${OWM_API_KEY}`)
+    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${MOVIES_API_KEY}`)
     const data = await response.json();
     res.json({ genres: data.genres });
 });
@@ -37,7 +37,7 @@ router.get('/genres', async (req, res) => {
 router.get('/topratedmovies', async (req, res) => {
     let topratedmovies = [];
     for (let i = 1; i <= 5; i++) {
-        const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US?api_key=${OWM_API_KEY}&page=${i}`);
+        const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US?api_key=${MOVIES_API_KEY}&page=${i}`);
         const data = await response.json();
         topratedmovies = topratedmovies.concat(data.results);
     }
@@ -49,7 +49,7 @@ router.get('/topratedmovies', async (req, res) => {
 router.get('/topratedtv', async (req, res) => {
     let topratedtv = [];
     for (let i = 1; i <= 5; i++) {
-        const response = await fetch(`https://api.themoviedb.org/3/tv/top_rated?language=en-US?api_key=${OWM_API_KEY}&page=${i}`);
+        const response = await fetch(`https://api.themoviedb.org/3/tv/top_rated?language=en-US?api_key=${MOVIES_API_KEY}&page=${i}`);
         const data = await response.json();
         topratedtv = topratedtv.concat(data.results);
     }

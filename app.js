51964684425ee285b9b34b4ecfb2a4b60fac23d0
Your name <Your email>
2024-05-09 10:43:56 +0200

    c

--- app.js
+++ app.js
@@ -12,7 +12,29 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 const cors = require('cors');
-app.use(cors());
+// app.use(cors());
+
+const corsOptions = {
+    origin: function (origin, callback) {
+
+        const allowedOrigins = [
+            "http://localhost:3000",
+            "http://localhost:3001",
+            "https://my-movies-frontend-six.vercel.app",
+            "https://my-movies-frontend-1e0sv0xsr-lauras-projects-ba7cc8a8.vercel.app",
+
+        ];
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+};
+
+app.use(cors(corsOptions))
 
 app.use(logger('dev'));
 app.use(express.json());
